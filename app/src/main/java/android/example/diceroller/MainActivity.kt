871c604b2ce96f482ef5d76797d28a6a6b1830e6
Santiago Rhenals <santiagorhenals@gmail.com>
2020-04-05 17:39:33 +0200

    feat: Added a dice image to show the result

--- app/src/main/java/android/example/diceroller/MainActivity.kt
+++ app/src/main/java/android/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package android.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -19,9 +20,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView =  findViewById(R.id.result_text)
         // Generate a random number between 0 and 6
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        // Assigning the drawable result
+        val diceResult = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Finding the diceImage by its ID
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(diceResult)
     }
 }
